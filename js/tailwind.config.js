935c3ba5d052e5aff289e189a864271f6d5824f0
shaxmardan1998 <shaxmardan79@gmail.com>
2023-01-18 16:16:39 +0500

    index.html style.css and and tailwind.config.js

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          font: '#252B42',
+          txt: '#737373',
+          btn: '#FF6551',
+          input:'#F9F9F9',
+          sale: '#E74040'
+        },
+        screens:{
+          "xs":"0px",
+          "sm":"500px",
+          "md":"768px",
+          "lg":"1024px",
+      }
+      }
+    }
+  }
